9a8d18b585f2731a8c12cb26a9dc2dbc3f9c4172
Sean Madsen <sean@seanmadsen.com>
2017-06-09 13:54:34 -0600

    Add tests for LocaleTools

---
+++ tests/AppBundle/Utils/LocaleToolsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Tests\Utils;
+
+use AppBundle\Utils\LocaleTools;
+
+class LocaleToolsTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * @param string $languageCode
+   * @param string $localeCode
+   * @param string $expected
+   * @dataProvider languageNameInLocaleProvider
+   */
+  public function testGetLanguageNameInLocale($languageCode, $localeCode, $expected) {
+    $this->assertEquals($expected, LocaleTools::getLanguageNameInLocale($languageCode, $localeCode));
+  }
+
+  /**
+   * @return array
+   */
+  public function languageNameInLocaleProvider() {
+    return [
+      [
+        'es',
+        'en',
+        'Spanish',
+      ],
+      [
+        'es',
+        'es',
+        'español',
+      ],
+    ];
+  }
+
+  /**
+   * @param string $languageCode
+   * @dataProvider codeProvider
+   */
+  public function testCodeIsValid($languageCode, $expected) {
+    $this->assertEquals($expected, LocaleTools::codeIsValid($languageCode));
+  }
+
+  public function codeProvider() {
+    $validCodes = ['en', 'es', 'fr'];
+    $invalidCodes = ['', NULL, 0, 'English', 'english', 'qq', '00', 'en-US'];
+
+    $validCodes = array_map(function ($i) {
+      return [$i, TRUE];
+    }, $validCodes);
+    $invalidCodes = array_map(function ($i) {
+      return [$i, FALSE];
+    }, $invalidCodes);
+
+    return array_merge($validCodes, $invalidCodes);
+  }
+
+}
